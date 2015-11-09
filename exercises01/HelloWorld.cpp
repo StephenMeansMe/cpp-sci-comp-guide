da4e8600d0c3c09cb79dd38ff4ebda44b970cd2c
StephenMeansMe <me+git@stephenmeansme.com>
2015-11-08 23:44:36 -0800

    Add source files of chapter 1 exercises

---
+++ exercises01/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* argv[]){
+	/*
+	 * This is a multi-line comment! Yay!
+	 *
+	 * Okay we're done here'
+	 */
+
+	 // Print "Hello World" to console
+	 std::cout << "Hello World!\n";
+
+	 return 0;
+	}
